7fc8f3a35fbd7178887e85e4217e273737bdfbb1
maythazinaung10 <maythazinaung04@gmial.com>
2026-07-01 13:42:58 +0630

    Configuration 2

--- scentoria-backend/routes/web.php
+++ scentoria-backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
 });

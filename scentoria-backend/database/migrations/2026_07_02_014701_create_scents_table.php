6e80499167f6c8003c2af170d0046df830b99bb4
maythazinaung10 <maythazinaung04@gmial.com>
2026-07-03 16:10:00 +0630

    Table images

--- scentoria-backend/database/migrations/2026_07_02_014701_create_scents_table.php
+++ scentoria-backend/database/migrations/2026_07_02_014701_create_scents_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->text('description');
+            $table->string('image_url')->nullable();
             $table->timestamps();
         });
     }
--- scentoria-backend/database/migrations/2026_07_02_014840_create_notes_table.php
+++ scentoria-backend/database/migrations/2026_07_02_014840_create_notes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('icon_url')->nullable();
             $table->timestamps();
         });
     }

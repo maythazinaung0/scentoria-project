a813b875e686d59874093eef5156394622edacd6
maythazinaung0 <maythazinaung04@gmail.com>
2026-07-30 09:35:04 +0700

    admin login error

--- scentoria-backend/bootstrap/app.php
+++ scentoria-backend/bootstrap/app.php
@@ -15,11 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-        $middleware->alias([
-            'admin' => AdminMiddleware::class,
-        ]);
-    })
+    $middleware->statefulApi();
+    $middleware->alias([
+        'admin' => AdminMiddleware::class,
+    ]);
+    $middleware->redirectGuestsTo(fn () => null);
+})
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (InvalidSignatureException $e, $request) {
             return response()->view('emails.orders.link-expired', [], 403);
